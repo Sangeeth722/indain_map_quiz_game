9443a89bbb868c663e9b51799aa306a81c21a5de
Sangeeth722 <72011368+Sangeeth722@users.noreply.github.com>
2023-08-16 22:37:21 +0530

    Update main.py
    To create a new CSV file for the states that have not been guessed

--- main.py
+++ main.py
@@ -18,6 +18,17 @@ while len(list_answer) < 50:
 
     answer_input = screen.textinput(title=f"Guess the state{len(list_answer)}/{50}", prompt="What is the another state?").title()
 
+    if answer_input == "Exit":
+
+        #list comprehensions in Python
+        user_not_input_states = [item for item in all_state_list if item not in list_answer]
+
+
+        #To create a new CSV file for the states that have not been guessed
+        
+        new_data_file = pandas.DataFrame(user_not_input_states)
+        new_data_file.to_csv("new data.csv")
+
     if answer_input in all_state_list:
         find_state_data = data[data.state == answer_input]
         t = turtle.Turtle()
